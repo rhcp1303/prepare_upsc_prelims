10d512c1bce4a4382b3fde391187ee105c20b868
ankit anand <ankit.anand@ankits-MacBook-Air.local>
2025-04-01 18:42:31 +0530

    integrating knowledge graph for mcq generation

--- questions/helpers/generate_prelims_mcq_from_questions_helper.py
+++ questions/helpers/generate_prelims_mcq_from_questions_helper.py
@@ -13,6 +13,7 @@ from ..helpers.prompt_helpers.mock_mcq_prompt_helper.identify_features_question_
 from ..helpers.prompt_helpers.mock_mcq_prompt_helper.match_the_pairs_question_prompt_helper import \
     match_the_pairs_prompt
 import multiprocessing
+from ..helpers import get_named_entities_from_text_helper as neh, query_kg_helper as qkh
 
 os.environ["TOKENIZERS_PARALLELISM"] = "false"
 
@@ -21,6 +22,18 @@ api_key_2 = "AIzaSyC_w68KVtMCloF5V3NKAUBp6EdhqcA0ylw"
 api_key_3 = "AIzaSyAA39dIq31iDJR-i7mZRWEKhkVVIr1Bz4g"
 api_key_4 = "AIzaSyD0nx9rH7HhQZDpJrY0hOaOR9Xok4r-liM"
 api_key_5 = "AIzaSyBq2_GdMf0KhowSVSb0hn4Z_8B81kBewXY"
+
+
+def traverse_graph_to_get_content(source_content):
+    named_entities = neh.get_ner_labels_from_llm(source_content)
+    p = []
+    for entity in named_entities:
+        print(entity)
+        target_entity_text = entity['entity']
+        all_paths = qkh.get_paths_from_entity_any_label(target_entity_text, max_depth=1)
+        for path in all_paths:
+            p.append(path)
+    return p
 
 
 def worker(prompt, prefix, suffix, api_key, source_content, target_content):
@@ -40,9 +53,9 @@ def generate_mock_mcq(source_embeddings_path, target_embeddings_path, prefix):
         source_embeddings_path,
         embeddings=embeddings,
         allow_dangerous_deserialization=True)
-    target_vectorstore = FAISS.load_local(target_embeddings_path,
-                                          embeddings=embeddings,
-                                          allow_dangerous_deserialization=True)
+    # target_vectorstore = FAISS.load_local(target_embeddings_path,
+    #                                       embeddings=embeddings,
+    #                                       allow_dangerous_deserialization=True)
     list_of_doc_ids = list(source_vectorstore.index_to_docstore_id.values())
     print("-" * 20)
     print(len(list_of_doc_ids))
@@ -54,11 +67,11 @@ def generate_mock_mcq(source_embeddings_path, target_embeddings_path, prefix):
         source_content = docs[0]
         print(source_content)
 
-
-        searched_content = target_vectorstore.similarity_search(str(source_content), k=10)
-        target_content = ""
-        for doc in searched_content:
-            target_content += str(doc) + "\n"
+        # searched_content = target_vectorstore.similarity_search(str(source_content), k=10)
+        # target_content = ""
+        # for doc in searched_content:
+        #     target_content += str(doc) + "\n"
+        target_content = traverse_graph_to_get_content(source_content)
         for prompt, suffix, api_key in [(single_statement_question_prompt, "single_statement.txt", api_key_1),
                                         (two_statements_question_prompt, "two_statement.txt", api_key_2),
                                         (three_statements_question_prompt, "three_statement.txt", api_key_3),
